4e0ea8911f1c8895916a0f34b2b595437e2abcbd
JoeManyak <doncrafttv@gmail.com>
2022-01-05 00:11:30 +0200

    files_fix

---
+++ src/utility/graphql-requester.js
@@ -0,0 +1,59 @@
+class requester {
+    URL = "https://web3-app3.herokuapp.com/v1/graphql"
+    async fetchGraphQL(operationsDoc, operationName, variables) {
+        const result = await fetch(
+            this.URL,
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    query: operationsDoc,
+                    variables: variables,
+                    operationName: operationName
+                })
+            }
+        );
+
+        return await result.json();
+    }
+    fetchMyQuery(operationsDoc) {
+        return this.fetchGraphQL(
+            operationsDoc,
+            "MyQuery",
+            {}
+        );
+    }
+
+    async startFetchMyQuery(operationsDoc) {
+        const {errors, data} = await this.fetchMyQuery(operationsDoc);
+
+        if (errors) {
+            // handle those errors like a pro
+            console.error(errors);
+        }
+
+        // do something great with this precious data
+        return data
+    }
+
+    executeMyMutation(operationsDoc) {
+        return this.fetchGraphQL(
+            operationsDoc,
+            "MyMutation",
+            {}
+        );
+    }
+
+    async startExecuteMyMutation(operationsDoc) {
+        const { errors, data } = await this.executeMyMutation(operationsDoc);
+
+        if (errors) {
+            // handle those errors like a pro
+            console.error(errors);
+        }
+
+        // do something great with this precious data
+        return data;
+    }
+}
+
+export default new requester();
